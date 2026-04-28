3ba7f2b86ba8f7c4e5b48d6c60f9bbf996b6b027
stojanovic4 <stojanovicmilijana0404@gmail.com>
2026-04-27 20:45:23 -0400

    docstrings

--- AuthSystem.cpp
+++ AuthSystem.cpp
@@ -3,6 +3,12 @@
 #include <fstream>
 using namespace std;
 
+/* Prompts the user to enter their username and password.
+Calls login from UserProfile.cpp to verify the attempt.
+Returns true if login was succesful.
+Returns false if username or password was incorrect,
+or such .txt file doesn't exist.
+*/
 bool AuthSystem::handleLogin(UserProfile& profile) {
     string username, password;
     cout << "\n-- Login --\n";
@@ -14,6 +20,14 @@ bool AuthSystem::handleLogin(UserProfile& profile) {
     return profile.login(username, password);
 }
 
+/* Prompts the user to choose a username.
+Checks if the username already exists from existing .txt files.
+Prompts the user to choose a password.
+Prompts the user to enter their zip code.
+Creates a new .txt file and calls login() from UserProfile.cpp.
+Returns true if an account was successfully created and the user logged in.
+Returns false if the username is taken.
+*/
 bool AuthSystem::handleSignup(UserProfile& profile) {
     string username, password, zip;
     cout << "\n-- Sign Up --\n";
